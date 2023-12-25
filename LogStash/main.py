b2dce4ee6b4daf24ad517b7a531f871f9d0a02a9
jmpleo <totrash@duck.com>
2023-12-25 14:00:09 +0300

    upd docker config

--- LogStash/main.py
+++ LogStash/main.py
@@ -3,10 +3,12 @@ import auth
 import searchf
 
 def main():
+    print("log scaning...")
     auth.main()
     access.main()
     searchf.main()
+    print("log checked...")
 
 if __name__ == "__main__":
     main()
-    
+
